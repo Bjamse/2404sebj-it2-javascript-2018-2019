fd8fb7856571c12481cb22597d6154e333e8d95e
bjamse@desktop <16bjorn@gmail.com>
2019-02-05 00:02:40 +0100

    bytta navn på mappe

---
+++ oppgaver i timen/Ekstraoppgaver/ONE MAN PONG/resurser/js.js
@@ -0,0 +1,8 @@
+// todo
+// loop bevegelse: forandre rettning ved kræsj: tell poeng ved treff
+// skjekk etter tastetrykk: flytt rekkert opp elller ned
+window.onload = boot;
+
+function boot(){
+    setInterval()
+}
